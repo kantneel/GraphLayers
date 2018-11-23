087e3214fae261a10ed931b8b3de3392a78f5c3d
Neel Kant <kantneel@gmail.com>
2018-11-23 13:19:31 -0800

    cleared out old Layer definition

--- framework/layer.py
+++ framework/layer.py
@@ -5,25 +5,12 @@ class GraphLayer(ABC):
     # https://github.com/tensorflow/tensorflow/blob/r1.12/tensorflow/python/ops/rnn_cell_impl.py#L174
     # https://github.com/tensorflow/tensorflow/blob/r1.12/tensorflow/python/keras/engine/base_layer.py#L71
 
-    def __init__(self, num_nodes, node_embed_size, adj_matrix, **kwargs):
-        """
-        Args - all are tf.tensors/ops/placeholders
-
-        - num_nodes: number of nodes this layer handles
-        - node_embed_size: dimension of node embeddings
-        - adj_matrix ([num_nodes x num_nodes]):
-            which nodes are connected to one another in directed edges
-        """
-
-        """
-        node label embed dim
-        edge label embed dim
-        node embed dim
-        """
-        self.num_nodes = num_nodes
-        self.node_embed_size = node_embed_size
-        self.adj_matrix = adj_matrix
-        self.out_shape = self.compute_output_shape()
+    def __init__(self, node_embed_dim, node_label_embed_dim,
+                 edge_label_embed_dim, output_embed_dim, **kwargs):
+        self.node_embed_dim = node_embed_dim
+        self.node_label_embed_dim = node_label_embed_dim
+        self.edge_label_embed_dim = edge_label_embed_dim
+        self.output_embed_dim = output_embed_dim
 
     def __str__(self):
         pass
