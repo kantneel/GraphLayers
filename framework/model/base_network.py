292f739756beec0c193df3815eed4520a07246c6
Neel Kant <kantneel@gmail.com>
2018-12-01 14:05:59 -0800

    fixed imports and also merged ModelManager into GraphNetwork - building, loading, saving

--- framework/model/base_network.py
+++ framework/model/base_network.py
@@ -1,5 +1,7 @@
 import sys
 sys.path.append('../../')
+import pickle
+import tensorflow as tf
 from framework.utils.io import IndexedFileReader, IndexedFileWriter
 
 class GraphNetwork(object):
@@ -11,9 +13,104 @@ class GraphNetwork(object):
         self.layers.append(new_layer)
         # check that layer properties are compatible
 
-    def __call__(self, placeholders):
+    def make_model(self, placeholders):
         results = placeholders
         for layer in self.layers:
             results = layer(results)
 
         return results
+
+    def build_graph_model(self, placeholders, mode='training', restore_file=None):
+        possible_modes = ['training', 'testing', 'inference']
+        if mode not in possible_modes:
+            raise NotImplementedError("Mode has to be one of {}".format(", ".join(possible_modes)))
+
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.graph = tf.Graph()
+        self.sess = tf.Session(graph=self.graph, config=config)
+        with self.graph.as_default():
+            self.make_model(placeholders)
+            self.ops = {}
+            if mode == 'training':
+                self.make_train_step()
+
+            if restore_file is None:
+                self.initialize_model()
+            else:
+                self.restore_model(restore_file)
+
+    def initialize_model(self):
+        init_op = tf.group(tf.global_variables_initializer(),
+                           tf.local_variables_initializer())
+        self.sess.run(init_op)
+
+    def restore_model(self, path):
+        debug = self.args.get('debug', False)
+        if debug:
+            print("Restoring weights from file %s." % path)
+        with open(path, 'rb') as in_file:
+            data_to_load = pickle.load(in_file)
+
+        variables_to_initialize = []
+        with tf.name_scope("restore"):
+            restore_ops = []
+            used_vars = set()
+            for variable in self.sess.graph.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
+                used_vars.add(variable.name)
+                if variable.name in data_to_load['weights']:
+                    if debug:
+                        print("Restoring weights for %s" % variable.name)
+                    restore_ops.append(variable.assign(data_to_load['weights'][variable.name]))
+                else:
+                    if debug:
+                        print('Freshly initializing %s since no saved value was found.' % variable.name)
+                    variables_to_initialize.append(variable)
+
+            if debug:
+                for var_name in data_to_load['weights']:
+                    if var_name not in used_vars:
+                        print('Saved weights for %s not used by model.' % var_name)
+
+            restore_ops.append(tf.variables_initializer(variables_to_initialize))
+            self.model.sess.run(restore_ops)
+
+    def save_model(self, path):
+        weights_to_save = {}
+        for variable in self.sess.graph.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
+            assert variable.name not in weights_to_save
+            weights_to_save[variable.name] = self.sess.run(variable)
+
+        data_to_save = {
+            "weights": weights_to_save
+        }
+
+        with open(path, 'wb') as out_file:
+            pickle.dump(data_to_save, out_file, pickle.HIGHEST_PROTOCOL)
+
+    def make_train_step(self):
+        trainable_vars = self.sess.graph.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+        #if self.params.args.freeze_graph_model:
+        if False:
+            graph_vars = set(self.sess.graph.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="graph_model"))
+            filtered_vars = []
+            for var in trainable_vars:
+                if var not in graph_vars:
+                    filtered_vars.append(var)
+                else:
+                    print("Freezing weights of variable %s." % var.name)
+            trainable_vars = filtered_vars
+
+        optimizer = tf.train.AdamOptimizer(self.params['lr'])
+        grads_and_vars = optimizer.compute_gradients(self.ops['loss'], var_list=trainable_vars)
+        clipped_grads = []
+        for grad, var in grads_and_vars:
+            if grad is not None:
+                clipped_grads.append((tf.clip_by_norm(grad, self.params['clamp_gradient_norm']), var))
+            else:
+                clipped_grads.append((grad, var))
+
+        self.ops['train_step'] = optimizer.apply_gradients(clipped_grads)
+
+        # Initialize newly-introduced variables:
+        self.sess.run(tf.local_variables_initializer())
