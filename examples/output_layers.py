1591a5bcd9dbb90fdaa89b0845f000f5d62ad0c9
Neel Kant <kantneel@gmail.com>
2018-12-01 14:06:30 -0800

    created DenseOutputLayer

---
+++ examples/output_layers.py
@@ -0,0 +1,50 @@
+import sys
+sys.path.append('../../')
+import tensorflow as tf
+import numpy as np
+from framework.layer import GraphLayer
+
+class DenseOutputLayer(GraphLayer):
+    def __init__(self, layer_params, network_params,
+                 hidden_units_list,
+                 num_classes,
+                 activation='relu',
+                 name='dense_output')
+
+    super().__init__(layer_params, network_params, name)
+    self.hidden_units_list = hidden_units_list
+    self.num_classes = num_classes
+    self.activation = eval('tf.nn.{0}'.format(activation))
+
+    self.create_weights()
+
+    def create_weights(self):
+        inp_size = self.layer_params.node_embed_size
+        self.dense_layers = []
+        with tf.variable_scope(self.name):
+            for i, hidden_size in enumerate(self.hidden_units_list):
+                self.dense_layers.append(
+                    tf.layers.Dense(hidden_size, self.activation,
+                                    name='layer_{0}'.format(i)))
+
+            # producing logits / regression values
+            self.dense_layers.append(
+                tf.layers.Dense(num_classes, name='final'))
+
+    def __call__(self, placeholders):
+        # might be graph embeds but it's okay
+        results = placeholders.input_node_embeds
+        for layer in self.dense_layers:
+            results = layer(results)
+
+        placeholders.input_node_embeds = results
+        return placeholders
+
+class SoftmaxLossLayer(GraphLayer):
+    def __init__(self, layer_params, network_params,
+                 name='softmax_loss'):
+        super().__init__(layer_params, network_params, name)
+
+    def __call__(self, placeholders):
+        pass
+
