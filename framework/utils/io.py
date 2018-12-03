57ae7f98a1aaaea31cd8e52492bc9830286e9e65
Neel Kant <kantneel@gmail.com>
2018-12-02 16:43:12 -0800

    added ThreadedIterator class

--- framework/utils/io.py
+++ framework/utils/io.py
@@ -68,3 +68,25 @@ class IndexedFileReader:
                 batch = [self.__getitem__(j) for j in range(i, min(total, i+batch_size))]
                 for res in p.imap(fn, batch):
                     yield res
+
+class ThreadedIterator:
+    """An iterator object that computes its elements in a parallel thread to be ready to be consumed.
+    The iterator should *not* return None"""
+
+    def __init__(self, original_iterator, max_queue_size=2):
+        self.__queue = queue.Queue(maxsize=max_queue_size)
+        self.__thread = threading.Thread(target=lambda: self.worker(original_iterator))
+        self.__thread.start()
+
+    def worker(self, original_iterator):
+        for element in original_iterator:
+            assert element is not None, 'By convention, iterator elements much not be None'
+            self.__queue.put(element, block=True)
+        self.__queue.put(None, block=True)
+
+    def __iter__(self):
+        next_element = self.__queue.get(block=True)
+        while next_element is not None:
+            yield next_element
+            next_element = self.__queue.get(block=True)
+        self.__thread.join()
